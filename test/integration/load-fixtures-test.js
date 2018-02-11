43e1c2f2d5c96d55322f3193bf4f67a1388c8dad
Gregor Martynus <gregor@martynus.net>
2018-02-11 12:36:03 -0800

    test: initial version

---
+++ test/integration/load-fixtures-test.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const supertest = require('supertest')
+const {test} = require('tap')
+
+const middleware = require('../..')
+
+test('create fixture success', t => {
+  const app = express()
+  app.use(middleware({
+    logLevel: 'error',
+    ttl: 1000
+  }))
+
+  supertest(app)
+    .post('/fixtures')
+    .send({scenario: 'get-repository'})
+    .then(response => {
+      const {id, url} = response.body
+      t.ok(id)
+      t.is(url, 'http://localhost:3000/api.github.com')
+      t.end()
+    })
+    .catch(t.error)
+})
+
+test('create fixture error', t => {
+  const app = express()
+  app.use(middleware({
+    logLevel: 'error',
+    ttl: 1
+  }))
+
+  supertest(app)
+    .post('/fixtures')
+    .send({scenario: 'nope'})
+    .then((response) => {
+      t.is(response.status, 400)
+      t.is(response.body.error, 'Scenario "nope" not found')
+      t.end()
+    })
+    .catch(t.error)
+})
+
+test('create fixture with custom url', t => {
+  const app = express()
+  app.use(middleware({
+    logLevel: 'error',
+    ttl: 1000,
+    fixturesUrl: 'https://deployment-123.my-fixtures.com'
+  }))
+
+  supertest(app)
+    .post('/fixtures')
+    .send({scenario: 'get-repository'})
+    .then(response => {
+      const {id, url} = response.body
+      t.ok(id)
+      t.is(url, 'https://deployment-123.my-fixtures.com/api.github.com')
+      t.end()
+    })
+    .catch(t.error)
+})
---
+++ test/integration/request-errors-test.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const supertest = require('supertest')
+const {test} = require('tap')
+
+const middleware = require('../..')
+
+test('request error: no matching fixture found', async t => {
+  const app = express()
+  app.use(middleware({
+    logLevel: 'silent',
+    ttl: 1000
+  }))
+
+  const agent = supertest(app)
+  const {body: {id}} = await agent
+    .post('/fixtures')
+    .send({scenario: 'create-file'})
+
+  const {status, body} = await agent
+    .put('/api.github.com/repos/octokit-fixture-org/create-file/contents/test.txt')
+    .set({
+      accept: 'application/vnd.github.v3+json',
+      Authorization: `token 0000000000000000000000000000000000000001`,
+      'x-fixtures-id': id
+    })
+    .send({
+      message: 'wrong message',
+      content: 'wrong content'
+    })
+
+  t.is(status, 404)
+  t.is(body.error, 'Nock: No match for request')
+
+  t.end()
+})
