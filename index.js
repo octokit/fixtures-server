4d814bd9f509de800264446373e7ea717a0db15e
Gregor Martynus <gregor@martynus.net>
2018-02-11 12:37:40 -0800

    feat: initial version

---
+++ index.js
@@ -0,0 +1,67 @@
+module.exports = fixtureServereMiddleware
+
+const {parse: urlParse, resolve: urlResolve} = require('url')
+
+const _ = require('lodash')
+const bodyParser = require('body-parser')
+const cachimo = require('cachimo')
+const express = require('express')
+const fixtures = require('@octokit/fixtures')
+const Log = require('console-log-level')
+
+const additions = require('./lib/additions')
+const proxy = require('./lib/proxy')
+
+const DEFAULTS = require('./lib/defaults')
+
+function fixtureServereMiddleware (options) {
+  const middleware = express.Router()
+
+  const state = _.defaults(_.clone(options), DEFAULTS)
+
+  if (!state.fixturesUrl) {
+    state.fixturesUrl = `http://localhost:${state.port}`
+  }
+
+  state.cachimo = cachimo
+  state.log = Log({level: state.logLevel === 'silent' ? 'fatal' : state.logLevel})
+
+  middleware.post('/fixtures', bodyParser.json(), (request, response) => {
+    const id = Math.random().toString(36).substr(2)
+    const requestedFixture = state.fixtures[request.body.scenario]
+
+    if (!requestedFixture) {
+      return response.status(400).json({
+        error: `Scenario "${request.body.scenario}" not found`
+      })
+    }
+
+    const mock = fixtures.mock(requestedFixture, fixture => additions(state, {id, fixture}))
+
+    cachimo
+      .put(id, mock, state.ttl)
+      .then(() => {
+        state.log.debug(`Deleted fixtures "${id}" (${mock.pending().length} pending)`)
+      })
+      // throws error if key was deleted before timeout, safe to ignore
+      .catch(() => {})
+
+    response.status(201).json({
+      id,
+      url: urlResolve(state.fixturesUrl, urlParse(requestedFixture[0].scope).hostname)
+    })
+  })
+
+  // load proxies for all unique scope URLs in fixtures
+  _.chain(state.fixtures)
+    .values()
+    .flatten()
+    .map('scope')
+    .uniq()
+    // remove default ports for http / https, they cause problems for the proxy
+    .map(url => url.replace(/:(80|443)$/, ''))
+    .forEach(target => middleware.use(proxy(state, {target})))
+    .value()
+
+  return middleware
+}
