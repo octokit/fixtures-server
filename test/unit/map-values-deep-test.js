7e6132eb055f5239a8e0607816e141626d9b73d1
Gregor Martynus <gregor@martynus.net>
2018-02-12 00:31:57 -0800

    test: leave arrays in fixtures intact

---
+++ test/unit/map-values-deep-test.js
@@ -0,0 +1,9 @@
+const {test} = require('tap')
+const mapValuesDeep = require('../../lib/map-values-deep')
+
+test('mapValuesDeep leaves arrays intact (#5)', t => {
+  const result = mapValuesDeep([1, 2], () => 0)
+  t.ok(Array.isArray(result))
+
+  t.end()
+})
