24344892a76ddeb0b1a08d7064023a6b89f56bf2
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-02-18 10:47:01 -0500

    spelling: server
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- bin/server.js
+++ bin/server.js
@@ -5,7 +5,7 @@ import express from "express";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-import fixtureServereMiddleware from "../index.js";
+import fixtureServerMiddleware from "../index.js";
 import globTofixtures from "../lib/glob-to-fixtures.js ";
 
 import DEFAULTS from "../lib/defaults.js";
@@ -48,7 +48,7 @@ app.get("/ping", (request, response) => {
   response.json({ ok: true });
 });
 app.use(
-  fixtureServereMiddleware({
+  fixtureServerMiddleware({
     port: argv.port,
     fixturesUrl: argv["fixtures-url"],
     logLevel: argv["log-level"],
